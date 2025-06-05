ff309bbceb240f5f983b3807ed2d3a6cd7b13b64
demo <ncshalini91@gmail.com>
2025-06-05 19:41:13 +0530

    shalini

---
+++ shalini/src/main/java/Nucot1/shalini/App.java
@@ -0,0 +1,13 @@
+package Nucot1.shalini;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ shalini/src/main/java/Nucot1/shalini/workspace1.java
@@ -0,0 +1,11 @@
+package Nucot1.shalini;
+
+public class workspace1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("workspace1");
+
+	}
+
+}
